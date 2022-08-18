17b5c386c00b0dab2f726961fa943b319bca6e53
Varun Chaubey <varunchaubey4@gmail.com>
2022-08-18 21:34:58 +0530

    added motion animation to the ball

--- Ball.js
+++ Ball.js
@@ -1,10 +1,14 @@
+const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.00001;
+
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem;
+        this.reset();
     }
 
     get x() {
-        return getComputedStyle(this.ballElem).getPropertyValue("--x"); 
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x")); 
     }
 
     set x(value) {
@@ -12,15 +16,45 @@ export default class Ball {
     }
 
     get y() {
-        return getComputedStyle(this.ballElem).getPropertyValue("--y"); 
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y")); 
     }
 
     set y(value) {
         this.ballElem.style.setProperty("--y", value);
     }
 
-    update(delta){
-        this.x = 5;
-        this.y = 15;
+    rect() {
+        return this.ballElem.getBoundingClientRect();
     }
+
+    reset() {
+        this.x = 50;
+        this.y = 50;
+        this.direction = { x: 0 };
+        // <0.2 because otherwise ball will only move vertically only and <0.9 will move ball only horizontally.
+        while(Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9) {
+            const heading = randomNumberBetween(0, 2 * Math.PI);
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading)};
+        }
+        this.velocity = INITIAL_VELOCITY;
+    }
+
+    update(delta){
+        this.x += this.direction.x * this.velocity * delta;
+        this.y += this.direction.y * this.velocity * delta;
+        this.velocity += VELOCITY_INCREASE * delta;
+        const rect = this.rect();
+
+        if(rect.bottom >= window.innerHeight || rect.top <=0) {
+            this.direction.y *= -1;
+        }
+
+        if(rect.right >= window.innerWidth || rect.left <=0) {
+            this.direction.x *= -1;
+        }
+    }
+}
+
+function randomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min;
 }
