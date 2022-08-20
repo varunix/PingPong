a19be728d98b5c05870aa70403b737f485715f1b
Varun Chaubey <varunchaubey4@gmail.com>
2022-08-20 08:08:07 +0530

    added code for computer- paddle

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 import Ball from './Ball.js';
+import Paddle from './Paddle.js';
 
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 let lastTime;
 function update(time){
     if (lastTime != null) {
         const delta =  time - lastTime;
-        ball.update(delta);
+        // ball.update(delta);
+        computerPaddle.update(delta, ball.y);
     }
     lastTime = time;
     window.requestAnimationFrame(update);
 }
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100;
+});
 
 window.requestAnimationFrame(update);
